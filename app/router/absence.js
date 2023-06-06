97782ac4625dde829be3e640c77722ebd420a322
syaifudin alkatiri <sfnalkatiri@gmail.com>
2023-06-06 17:25:38 +0700

    middleware auth

--- app/router/absence.js
+++ app/router/absence.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const route = express.Router();
 const absenceController = require("../api/absence");
-const absence = require("../api/absence");
+const verifyJwtToken = require("../api/verifyJwtToken");
 
 route.get("/", absenceController.getAbsen);
-route.post("/checkin", absenceController.checkin);
+route.post("/checkin", [verifyJwtToken.verifyToken], absenceController.checkin);
 
 module.exports = route;
